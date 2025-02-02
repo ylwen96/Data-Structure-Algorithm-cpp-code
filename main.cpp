15e5f0c0835a8cdb6b97f81687bf759b969ad024
yuelin-wen <wenyuelinca@gmail.com>
2025-02-02 14:18:45 -0500

    init

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Data-Structure-Algorithm-cpp-code
+//
+//  Created by Yuelin Wen on 2025-02-02.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
